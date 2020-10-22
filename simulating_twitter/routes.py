4c3bd8775ce97f4183b772a3343110995cf6f02c
Renee <shanju.chou@gmail.com>
2020-10-23 00:55:58 +0800

    added, mapped sidebar links

--- simulating_twitter/routes.py
+++ simulating_twitter/routes.py
@@ -1,7 +1,58 @@
 from simulating_twitter import app
-from flask import render_template
+from flask import render_template, redirect
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('layout.html')
+    return render_template('home.html')
+
+
+@app.route('/explore')
+def explore():
+    return render_template('explore.html')
+
+
+
+@app.route('/notifications')
+def notifications():
+    return render_template('notifications.html')
+
+
+
+@app.route('/messages')
+def messages():
+    return render_template('messages.html')
+
+
+
+@app.route('/bookmarks')
+def bookmarks():
+    return render_template('bookmarks.html')
+
+
+
+@app.route('/profile/lists')
+def lists():
+    return render_template('lists.html')
+
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+
+@app.route('/compose/tweet')
+def tweet():
+    return render_template('tweet.html')
+
+
+@app.route('/account/add')
+def account_add():
+    return render_template('account_add.html')
+
+
+@app.route('/account/switch')
+def account_switch():
+    return render_template('account_switch.html')
