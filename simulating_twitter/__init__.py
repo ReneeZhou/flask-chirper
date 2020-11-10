925c27d9d509574d3ea995e59d81169dddfd699a
Renee <shanju.chou@gmail.com>
2020-11-10 18:33:36 +0800

    added Bcrypt & LoginManager to package

--- simulating_twitter/__init__.py
+++ simulating_twitter/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 # instance_relative_config allows separation of private info
 app = Flask(__name__, instance_relative_config = True)
@@ -16,5 +17,12 @@ app.config.from_pyfile('config.py')
 
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+# telling the extension where the login route is located when login_required
+login_manager.login_view = 'login'
+
+# customize message category
+# login_manager.login_message_category ='info' 
 
 from simulating_twitter import routes
