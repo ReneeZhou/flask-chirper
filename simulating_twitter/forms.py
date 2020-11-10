81a410f4e72b69566ebd2c55b1a648f8a0d96ba3
Renee <shanju.chou@gmail.com>
2020-11-10 18:30:31 +0800

    added custom form validation

--- simulating_twitter/forms.py
+++ simulating_twitter/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from simulating_twitter.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', \
@@ -17,10 +17,25 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
 
+    # create a custom validation
+    # check whether username exists already
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one. ')
+    
+    # check whether email is already used
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one. ')
+
+
 class LoginForm(FlaskForm):
     email = StringField('Email', \
         validators = [DataRequired(), Email()])
     password = PasswordField('Password',\
         validators = [DataRequired()])
-    remember = BooleanField('Remember Me')
+    # remember = BooleanField('Remember Me') 
+    # we will remember user by default unless they sign out manually
     submit = SubmitField('Log In')
