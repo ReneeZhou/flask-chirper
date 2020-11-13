698b6d33d1fc9a420297522a104a7a7f139fd610
Renee <shanju.chou@gmail.com>
2020-11-13 18:26:38 +0800

    added new fields for db User model

--- simulating_twitter/models.py
+++ simulating_twitter/models.py
@@ -11,17 +11,25 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), unique = True, nullable = False)
-    email = db.Column(db.String(120), unique = True, nullable = False)
-    image_file = db.Column(db.String(20), nullable = False, default = 'default_profile.png')
+    handle = db.Column(db.String(20), nullable = False, unique = True)  # handle
+    name = db.Column(db.String(50), nullable = False)                   # profile name
+    email = db.Column(db.String(120), nullable = False, unique = True)
     password = db.Column(db.String(60), nullable = False)
+
+    image_file = db.Column(db.String(20), nullable = False, default = 'default_profile.png')
+    background_image = db.Column(db.String(20))  # db will default Null because nullable
+    bio = db.Column(db.String(160))              # give it a string None so it can find labels??
+    location = db.Column(db.String(30))
+    website = db.Column(db.String(100))
+
     posts = db.relationship('Post', backref = 'author', lazy = True) 
     # referring to Post class
 
     def __repr__(self):
-        return f'User("{self.username}", "{self.email}", "{self.image_file}")'
+        return f'User("{self.name}", "{self.email}", "{self.image_file}")'
      
 
 
