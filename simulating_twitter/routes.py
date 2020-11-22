18039fa23858bbf2c7490470dd339915d4cfa86a
Renee <shanju.chou@gmail.com>
2020-11-22 11:51:17 +0800

    showed post.date_posted dynamically

--- simulating_twitter/routes.py
+++ simulating_twitter/routes.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from secrets import token_urlsafe, token_hex
 from PIL import Image
 from flask import render_template, redirect, url_for, flash, request
@@ -85,15 +86,38 @@ def logout():
     return redirect(url_for('home_notauth'))
 
 
+def show_time(date_posted):
+    t = datetime.utcnow() - date_posted
+    if t.seconds < 59:
+        return f'{t.seconds}s'
+    elif 50 <= t.seconds < 3600:
+        return f'{t.seconds//60}m'
+    elif t.days < 1:
+        return f'{t.seconds//3600}h'
+    elif date_posted.year == datetime.utcnow().year:
+        return date_posted.strftime('%d %b')
+    else: 
+        return date_posted.strftime('%d %b %Y')
+
+
 # @app.rounte('/')
-@app.route('/home')
+@app.route('/home', methods = ['GET', 'POST'])
 @login_required
 def home():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(content = form.content.data, author = current_user)
+        db.session.add(post)
+        db.session.commit()
+        return redirect('home')
+
     posts = Post.query.all()
+    for post in posts:
+        post.show = show_time(post.date_posted)
     profile_image = url_for('static', filename = 'img/profile_pics/' + current_user.profile_image)
     # .profile_image is a column from the User model 
     return render_template('home.html', \
-        posts = posts, profile_image = profile_image)
+        profile_image = profile_image, form = form, posts = posts)
 
 
 @app.route('/explore', methods = ['GET', 'POST'])
