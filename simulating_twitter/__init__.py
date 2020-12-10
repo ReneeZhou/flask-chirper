cacd70acbb8c1399b8a8fecad5b09b197fbe2ef8
Renee <shanju.chou@gmail.com>
2020-12-10 14:54:17 +0800

    added mail config

--- simulating_twitter/__init__.py
+++ simulating_twitter/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 # instance_relative_config allows separation of private info
 app = Flask(__name__, instance_relative_config = True)
@@ -25,4 +27,11 @@ login_manager.login_view = 'login'
 # customize message category
 # login_manager.login_message_category ='info' 
 
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = '587'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+# initialize the extension with the config set
+mail = Mail(app)
 from simulating_twitter import routes
