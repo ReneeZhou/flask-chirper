38c1f756f8bc8f299a0f138c79a978b83ca3bbfb
Renee <shanju.chou@gmail.com>
2020-12-14 13:37:06 +0800

    restructured app - error pages

---
+++ simulating_twitter/error/handlers.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template
+
+
+error = Blueprint('error', __name__)
+
+
+# .errorhandler will only be active within this blueprint
+# whereas .app_errorhandler works across the entire application
+@error.app_errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
+
+
+@error.app_errorhandler(403)
+def forbidden(error):
+    return render_template('errors/403.html'), 403
+
+
+@error.app_errorhandler(500)
+def forbidden(error):
+    return render_template('errors/403.html'), 500
--- simulating_twitter/error/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
-
-error = Blueprint('error', __name__)
-
-@error.errorhandler(404)
-@login_required
-def page_not_found(error_status):
-    return render_template('404.html', error_status = 404)
