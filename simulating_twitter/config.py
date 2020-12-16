ccd376022d86bf053890e4fa405261c203b60569
Renee <shanju.chou@gmail.com>
2020-12-16 16:53:25 +0800

    debugged - fixed typo

--- simulating_twitter/config.py
+++ simulating_twitter/config.py
@@ -2,7 +2,8 @@ import os
 
 class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = '587'
-    MAIL_USE_TSL = True
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
