625cffbb9144f4c8e6c419ff4606b3c9cd0027c3
Renee <shanju.chou@gmail.com>
2020-12-29 23:15:45 +0800

    added a follower table and relationship in User for follower feature

--- simulating_twitter/models.py
+++ simulating_twitter/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app, request
+from flask import current_app
 from flask_login import UserMixin
 from simulating_twitter import db, login_manager    # so we can use app's secret key in the serializer
 
@@ -17,6 +17,17 @@ def load_user(user_id):
 # don't need @declared_attr as it doesn't involve ForeignKey
 class TimestampMixin:    # causing error if inherit from db.Model here
     created_at = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+
+
+
+
+# not declaring this table as a model as this is an auxiliary table
+# which has no data other than foreign keys
+follower = db.Table(
+    'followers', 
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
 
 
 
@@ -40,11 +51,15 @@ class User(db.Model, UserMixin, TimestampMixin):
     location = db.Column(db.String(30), nullable = False, default = '')
     website = db.Column(db.String(100), nullable = False, default = '')
 
-    posts = db.relationship('Post', backref = 'author', lazy = True) 
+    posts = db.relationship('Post', backref = 'author', lazy = True)             # same as lazy = 'select' 
     # referring to Post class
 
 
-    liked = db.relationship('PostLike', backref = 'liker', lazy = 'dynamic')
+    followed = db.relationship('User', secondary = follower, \
+        primaryjoin = (follower.c.follower_id == id), \
+        secondaryjoin = (follower.c.followed_id == id), \
+        backref = db.backref('follower', lazy = 'dynamic'), \
+        lazy = 'dynamic')
 
 
     # method to create token for serializer
@@ -88,9 +103,3 @@ class Post(db.Model, TimestampMixin):
 
     def __repr__(self):
         return f'Post("{self.user_id}", "{self.post_id}", "{self.created_at}", f"{self.content}")'
-
-
-class PostLike(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.Integer, db.ForeignerKey('user.id'))
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
