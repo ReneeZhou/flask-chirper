26a233e6609e8372c037d60fdd73f4892ee20273
Renee <shanju.chou@gmail.com>
2020-12-31 15:00:27 +0800

    showed posts only from following users

--- simulating_twitter/main/routes.py
+++ simulating_twitter/main/routes.py
@@ -20,7 +20,9 @@ def home():
         db.session.commit()
         return redirect(url_for('main.home'))
 
-    posts = Post.query.order_by(Post.created_at.desc()).all()
+    # posts = Post.query.order_by(Post.created_at.desc()).all()
+    posts = current_user.following_post().all()
+
     for post in posts:
         post.show = show_time(post.created_at)
     # .profile_image is a column from the User model 
