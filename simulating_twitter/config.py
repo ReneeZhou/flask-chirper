fc321694595adb9444c0a5ec2e13ca3e82e8647c
Renee <shanju.chou@gmail.com>
2021-04-16 17:00:02 +0800

    updated config keys

--- simulating_twitter/config.py
+++ simulating_twitter/config.py
@@ -4,5 +4,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('USER_EMAIL')
+    MAIL_PASSWORD = os.environ.get('USER_PASSWORD')
