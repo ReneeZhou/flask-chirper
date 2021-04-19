183a19ddf8379b3c903073276c0178120936187e
Renee <shanju.chou@gmail.com>
2021-04-19 17:12:20 +0800

    updated app config

--- simulating_twitter/config.py
+++ simulating_twitter/config.py
@@ -1,8 +1,25 @@
+from instance.config import SQLALCHEMY_DATABASE_URI
 import os
 
 class Config:
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///site.db'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('USER_EMAIL')
     MAIL_PASSWORD = os.environ.get('USER_PASSWORD')
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
