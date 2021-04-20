da535ece5a27a519560ad19f5c243300e2815b10
Renee <shanju.chou@gmail.com>
2021-04-20 16:59:13 +0800

    modified User model

--- simulating_twitter/models.py
+++ simulating_twitter/models.py
@@ -37,7 +37,7 @@ class TimestampMixin:    # causing error if inherit from db.Model here
 
 class User(db.Model, UserMixin, TimestampMixin):
     created_at_ip = db.Column(db.String, nullable = False)
-    id = db.Column(db.BIGINT(unsigned = True), primary_key = True)
+    id = db.Column(db.BIGINT.__init__(unsigned = True), primary_key = True)
     handle = db.Column(db.String(20), nullable = False, unique = True)          # handle
     name = db.Column(db.String(50), nullable = False)                           # profile name
     email = db.Column(db.String(120), nullable = False, unique = True)
